411dd4736148f7b0b5539fc44a3aad075339c75a
Nafiul Islam <32985815+Nafiuli131@users.noreply.github.com>
2023-05-11 09:29:30 +0600

    Add files via upload

---
+++ Configure_multiple_db/README.md
@@ -0,0 +1,1 @@
+"# Configure_Multiple_DB" 
---
+++ Configure_multiple_db/src/main/java/com/example/Configure_multiple_db/ConfigureMultipleDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.Configure_multiple_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigureMultipleDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigureMultipleDbApplication.class, args);
+	}
+
+}
